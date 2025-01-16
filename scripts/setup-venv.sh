374eb8d06e92d65285fd3791207984d73f180531
Brian Zenowich <bz@barrett.com>
2025-01-16 14:36:54 -0500

    Added python3-wheel to deps

--- scripts/setup-venv.sh
+++ scripts/setup-venv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update
-sudo apt install -y python3-dev python3-venv libgtk-3-dev
+sudo apt install -y python3-dev python3-venv python3-wheel libgtk-3-dev
 
 # Set up the virtual environment required for development & building
 python3 -m venv .
