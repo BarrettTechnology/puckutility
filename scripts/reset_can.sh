c7f3eb91595397dc23f5bd113d37837e35d8cf0e
Brian Zenowich <bz@barrett.com>
2025-01-16 15:19:47 -0500

    Edited socketcan setup to be more robust

--- scripts/reset_can.sh
+++ scripts/reset_can.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo ip link set can0 up type can restart-ms 100 bitrate 1000000
+sudo ip link set can0 down
+sudo ip link set can0 type can restart-ms 100 bitrate 1000000 
+sudo ip link set can0 txqueuelen 1000
+sudo ip link set can0 up
 echo "CAN port 0 has been reset"
 
--- scripts/setup-socketcan.sh
+++ scripts/setup-socketcan.sh
@@ -1,16 +1,17 @@
 sudo cp reset_can.sh /bin
 sudo cp 60-can.rules /etc/udev/rules.d
-sudo udevadm control --reload-rules && udevadm trigger
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 #sh install_pcan.sh # removing pcan usage
 
-if sudo grep can0 /etc/network/interfaces -q; then
+if grep can0 /etc/network/interfaces -q; then
   echo "can0 already installed"
   exit 1
 fi
 echo "Installing can0..."
-sudo echo "auto can0
+echo "auto can0
 iface can0 inet manual
     pre-up /sbin/ip link set can0 type can bitrate 1000000 txqueuelen 1000
     ip link set can0 txqueuelen 1000
     up /sbin/ip link set can0 up
-    down /sbin/ip link set can0 down" >> /etc/network/interfaces
+    down /sbin/ip link set can0 down" | sudo tee -a /etc/network/interfaces > /dev/null
