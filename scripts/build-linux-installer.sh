a1e878875bda183635b758a1421ea988229b5bd3
Bailey Noack <bn@barrett.com>
2025-05-16 12:51:34 -0400

    fixed linux / windows installer scripts to include missing files

--- scripts/build-linux-installer.sh
+++ scripts/build-linux-installer.sh
@@ -7,11 +7,11 @@ cp puck4.eds build/lin/PuckUtilityApp/
 cp flashloader.eds build/lin/PuckUtilityApp/
 cp scripts/reset_can.sh build/lin/PuckUtilityApp/
 cp scripts/60-can.rules build/lin/PuckUtilityApp/
-#cp canopen_runner.py build/lin/PuckUtilityApp/
-#cp flashp4.py build/lin/PuckUtilityApp/
+cp canopen_runner.py build/lin/PuckUtilityApp/
+cp flashp4.py build/lin/PuckUtilityApp/
 cp scripts/setup-socketcan.sh build/lin/PuckUtilityApp/
 cp scripts/install-ubuntu.sh build/lin/PuckUtilityApp/
 cp PuckUtilityApp.desktop build/lin/PuckUtilityApp/
-#cp -r ../firmware/ PuckUtilityApp/ 
+cp -r ../firmware/ PuckUtilityApp/ 
 cd build/lin
 zip -r ../PuckUtilityApp-lin.zip PuckUtilityApp 
--- scripts/build-win32-installer.sh
+++ scripts/build-win32-installer.sh
@@ -4,10 +4,10 @@ python -m PyInstaller --clean puckutilityapp.py --name PuckUtilityApp --distpath
 cp -r images build/win/PuckUtilityApp/
 cp -r config build/win/PuckUtilityApp/
 cp puck4.eds build/win/PuckUtilityApp/
-#cp flashp4.py build/win/PuckUtilityApp/
-#cp canopen_runner.py PuckUtilityApp/
-#cp flashloader.eds PuckUtilityApp/
-#cp -r firmware PuckUtilityApp/
+cp flashp4.py build/win/PuckUtilityApp/
+cp canopen_runner.py PuckUtilityApp/
+cp flashloader.eds PuckUtilityApp/
+cp -r firmware PuckUtilityApp/
 curl https://web.barrett.com/support/Puck_ControlLibrary/PeakOemDrv.exe -o PeakOemDrv.exe
 mv PeakOemDrv.exe build/win/PuckUtilityApp/
 cd build/win
